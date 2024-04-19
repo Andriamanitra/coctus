512f32bc9433e47af584143aa7a1979b4a0be43a
Andriamanitra <mikko77792@gmail.com>
2024-04-20 00:30:30 +0300

    teststub.py: do xABC,xABC,xABC instead of xAxAxA,xBxBxB,xCxCxC

--- scripts/teststub.py
+++ scripts/teststub.py
@@ -7,63 +7,58 @@ from os.path import expanduser, basename, splitext
 CLASH_DIR = expanduser("~/.local/share/clash/clashes")
 CLASH_EXE = "target/release/clash"
 
-def check_stubgen(*, clash_ids: list[str], lang: str, check_cmd: list[str]) -> dict:
+
+def check_stubgen(*, clash_ids: list[str], langs_to_check: dict[str, list[str]]) -> dict:
     """
     Checks that stubgen generates valid code for all given clash_ids.
     Returns the number of errors encountered.
-    check_cmd should be a command that reads the code from STDIN and exits non-zero
-    if the code is not valid.
+    langs_to_check should be a dict where keys are language names for the
+    `clash generate-stub` and values are commands that read the generated
+    code from STDIN and exit non-zero if the code is not valid.
 
     Example:
     ========
     check_stubgen(
         clash_ids=["682102420fbce0fce95e0ee56095ea2b9924"],
-        lang="c",
-        check_cmd=["gcc", "-fsyntax-only", "-x", "c", "-"]
+        langs_to_check={"c": ["gcc", "-fsyntax-only", "-x", "c", "-"]}
     )
     """
     SEPARATOR = "=" * 30
-    GENERATE_STUB = [CLASH_EXE, "generate-stub", lang]
-    n_skipped = 0
-    n_checked = 0
-    n_errors = 0
+    results = {lang: {"n_skipped": 0, "n_checked": 0, "n_errors": 0} for lang in langs_to_check}
 
     for cid in clash_ids:
         run([CLASH_EXE, "next", cid], capture_output=True)
 
-        run_stubgen = run(GENERATE_STUB, capture_output=True)
-        if run_stubgen.returncode != 0:
-            stderr = run_stubgen.stderr.decode("utf-8")
-            if "Clash provides no input stub generator" in stderr:
-                n_skipped += 1
+        for lang, check_cmd in langs_to_check.items():
+            res = results[lang]
+            run_stubgen = run([CLASH_EXE, "generate-stub", lang], capture_output=True)
+            if run_stubgen.returncode != 0:
+                stderr = run_stubgen.stderr.decode("utf-8")
+                if "Clash provides no input stub generator" in stderr:
+                    res["n_skipped"] += 1
+                else:
+                    res["n_checked"] += 1
+                    res["n_errors"] += 1
+                    print(f"\nStub generator for {lang} returned non-zero for clash {cid}")
+                    print(SEPARATOR)
+                    print(run_stubgen.stderr.decode("utf-8"))
+                    print(SEPARATOR)
+                    print()
             else:
-                n_checked += 1
-                n_errors += 1
-                print(f"\nStub generator returned non-zero for clash {cid}")
-                print(SEPARATOR)
-                print(run_stubgen.stderr.decode("utf-8"))
-                print(SEPARATOR)
-                print()
-            continue
+                run_check = run(check_cmd, input=run_stubgen.stdout, capture_output=True)
+                res["n_checked"] += 1
+                if run_check.returncode != 0:
+                    res["n_errors"] += 1
+                    print(f"\nError with {lang} stub for clash {cid}")
+                    print(f"https://www.codingame.com/contribute/view/{cid}")
+                    print(SEPARATOR)
+                    print(run_check.stderr.decode("utf-8"))
+                    print(SEPARATOR)
+                    print(run_stubgen.stdout.decode("utf-8"))
+                    print(SEPARATOR)
+                    print()
 
-        run_check = run(check_cmd, input=run_stubgen.stdout, capture_output=True)
-        n_checked += 1
-        if run_check.returncode != 0:
-            n_errors += 1
-            print(f"\nError with stub for clash {cid}")
-            print(f"https://www.codingame.com/contribute/view/{cid}")
-            print(SEPARATOR)
-            print(run_check.stderr.decode("utf-8"))
-            print(SEPARATOR)
-            print(run_stubgen.stdout.decode("utf-8"))
-            print(SEPARATOR)
-            print()
-
-    return {
-        "num_checked": n_checked,
-        "num_skipped": n_skipped,
-        "num_errors": n_errors
-    }
+    return results
 
 
 def main(n = None):
@@ -82,10 +77,8 @@ def main(n = None):
         "rust": ["rustc", "--emit=metadata", "-"],
     }
 
-    lang_results = {
-        lang: check_stubgen(clash_ids=clash_ids, lang=lang, check_cmd=check_cmd)
-        for lang, check_cmd in langs_to_check.items()
-    }
+    lang_results = check_stubgen(clash_ids=clash_ids, langs_to_check=langs_to_check)
+
     for lang, results in lang_results.items():
         print(f"{lang}: {results}")
 
